0c99c73f9d689ebe924952aea0aefc2cf82f47ea
evelynchen1229 <evelynchen1229@gmail.com>
2022-08-17 23:19:18 +0100

    remove index false and alias streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,14 @@
-import streamlit
+import streamlit as st
 import pandas as pd
 
-streamlit.title('My Mum\'s New Healthy Dinner')
-streamlit.header('Breakfast Favourites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+st.title('My Mum\'s New Healthy Dinner')
+st.header('Breakfast Favourites')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt') 
-streamlit.dataframe(my_fruit_list, index=False)
+st.dataframe(my_fruit_list))
