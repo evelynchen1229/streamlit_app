4d028ecf348aae23310f69666d1c95229eb60a34
evelynchen1229 <evelynchen1229@gmail.com>
2022-11-03 16:25:37 +0000

    delete extra bracket

--- dbt_scheduled_job_list.py
+++ dbt_scheduled_job_list.py
@@ -6,7 +6,7 @@ def show_job_list(conn):
     with conn.cursor() as my_cur:
         my_cur.execute(
                 ''' SELECT DISTINCT job_name
-                    FROM PROD.WORKSPACE_EVELYN_CHEN.EC_DBT_JOBS_SNAPSHOT)
+                    FROM PROD.WORKSPACE_EVELYN_CHEN.EC_DBT_JOBS_SNAPSHOT
                     WHERE snapshot_date = (SELECT MAX(snapshot_date) FROM PROD.WORKSPACE_EVELYN_CHEN.EC_DBT_JOBS_SNAPSHOT)
                         AND is_schedule = true
                     ORDER BY 1
