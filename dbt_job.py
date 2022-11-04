e090fb182740a2bde8c03e911ab7dbeeb6b4b059
evelynchen1229 <evelynchen1229@gmail.com>
2022-11-04 16:41:02 +0000

    amend connection

--- dbt_job.py
+++ dbt_job.py
@@ -1,37 +1,42 @@
 import requests
 import pandas as pd
 import numpy as np
+import os
 import snowflake.connector
+import sqlalchemy
+from sqlalchemy import create_engine
+from snowflake.sqlalchemy import URL
 import streamlit as st
-
 
 API_KEY = st.secrets['dbt_api_key']
 ACCOUNT_ID = 801
 
-DBT_JOB_PATTERN = 'https://cloud.getdbt.com/next/deploy/801/projects/1031/jobs/'
+dbt_job_pattern = 'https://cloud.getdbt.com/next/deploy/801/projects/1031/jobs/'
 
-SNOWFLAKE_DATABASE = 'prod'
-SNOWFLAKE_SCHEMA = 'workspace_evelyn_chen'
-SNOWFLAKE_TABLE = 'dbt_jobs_minoro'
-
-jobs = []
-job_names = []
-job_links = []
-is_scheduled = []
+snowflake_database = 'prod'
+snowflake_schema = 'workspace_evelyn_chen'
 
 # snowflake set up
-my_cnx = snowflake.connector.connect(**st.secrets['snowflake'])
+engine = create_engine(
+        URL(account = st.secrets['snowflake']['account'], 
+            user = st.secrets['snowflake']['user'],
+            password = st.secrets['snowflake'['password']), 
+            role = 'RL_DPT',
+            warehouse = 'WH_DPT_XS',
+            database = snowflake_database,
+            schema = snowflake_schema
+            )
+)
 
 def upload_to_snowflake(
         dataframe,
-        connection,
+        engine,
         table_name,
         truncate=False,
         create = True
         ):
 
-    with connection.cursor() as con:
-        print('connect')
+    with engine.connect() as con:
         if create:
             dataframe.to_sql(
                     name=table_name,
@@ -45,12 +50,12 @@ def upload_to_snowflake(
     con.close()
 
 def query_snowflake(
-        connection,
+        engine,
         table_name,
-        database = SNOWFLAKE_DATABASE,
-        schema = SNOWFLAKE_SCHEMA
+        database = snowflake_database,
+        schema = snowflake_schema
         ):
-    with connection.cursor() as con:
+    with engine.connect() as con:
         sql = (
                 f''' SELECT *
                     FROM {database}.{schema}.{table_name}
@@ -61,39 +66,42 @@ def query_snowflake(
     return df_job_description
 
 # get a list of all jobs in the project
-def dbt_job_extraction(account_id=ACCOUNT_ID, api_key=API_KEY, table=SNOWFLAKE_TABLE):
-    res = requests.get(
-            url = f"https://cloud.getdbt.com/api/v2/accounts/{account_id}/jobs/"
-            ,headers={'Authorization': f"Token {api_key}", 'Content-Type': 'application/json'},
-            params ={'project_id':1031}
+jobs = []
+job_names = []
+job_links = []
+is_scheduled = []
+res = requests.get(
+        url = f"https://cloud.getdbt.com/api/v2/accounts/{ACCOUNT_ID}/jobs/"
+        ,headers={'Authorization': f"Token {API_KEY}", 'Content-Type': 'application/json'},
+        params ={'project_id':1031}
+        )
+dic = res.json()
+job_list = dic.get('data')
+
+for job in job_list:
+    jobs.append(job['id'])
+    job_names.append(job['name'])
+    job_links.append(dbt_job_pattern+str(job['id'])) 
+    is_scheduled.append(job['triggers']['schedule'])
+job_dict = {
+        'job_id':jobs,
+        'job_name':job_names,
+        'job_link':job_links,
+        'is_scheduled':is_scheduled,
+        'description':None
+        }
+# generate dataframe
+df_current_job = pd.DataFrame(job_dict)
+cols = df_current_job.columns
+
+try:
+    df_job_description = query_snowflake(engine,'dbt_jobs_minoro') 
+    df_current_job_with_descripiton = df_current_job[cols[:-1]].merge(
+            df_job_description[['job_id','description']], on='job_id', how='left'
             )
-    dic = res.json()
-    job_list = dic.get('data')
+    upload_to_snowflake(df_current_job_with_descripiton,engine,'dbt_jobs_minoro')
+except:
+    print('table does not exist')
+    upload_to_snowflake(df_current_job,engine,'dbt_jobs_minoro')
 
-    for job in job_list:
-        jobs.append(job['id'])
-        job_names.append(job['name'])
-        job_links.append(DBT_JOB_PATTERN+str(job['id'])) 
-        is_scheduled.append(job['triggers']['schedule'])
-    job_dict = {
-            'job_id':jobs,
-            'job_name':job_names,
-            'job_link':job_links,
-            'is_scheduled':is_scheduled,
-            'description':None
-            }
-    # generate dataframe
-    df_current_job = pd.DataFrame(job_dict)
-    cols = df_current_job.columns
-
-    try:
-        df_job_description = query_snowflake(my_cnx,table) 
-        df_current_job_with_descripiton = df_current_job[cols[:-1]].merge(
-                df_job_description[['job_id','description']], on='job_id', how='left'
-                )
-        upload_to_snowflake(df_current_job_with_descripiton,my_cnx,table)
-    except:
-        print('table does not exist')
-        upload_to_snowflake(df_current_job,my_cnx,table)
-
-    engine.dispose()
+engine.dispose()
