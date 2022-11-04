972059fa9c3840322b185e50911f3599673870dc
evelynchen1229 <evelynchen1229@gmail.com>
2022-11-04 14:24:10 +0000

    chahnge sql query

--- dbt_scheduled_job_list.py
+++ dbt_scheduled_job_list.py
@@ -5,17 +5,20 @@ import snowflake.connector
 def show_job_list(conn):
     with conn.cursor() as my_cur:
         my_cur.execute(
-                ''' SELECT DISTINCT job_name
-                    FROM PROD.WORKSPACE_EVELYN_CHEN.EC_DBT_JOBS_SNAPSHOT
-                    WHERE snapshot_date = (SELECT MAX(snapshot_date) FROM PROD.WORKSPACE_EVELYN_CHEN.EC_DBT_JOBS_SNAPSHOT)
+                ''' SELECT DISTINCT job_name, job_link, description
+                    FROM PROD.WORKSPACE_EVELYN_CHEN.DBT_JOBS_MINORO
+                    WHERE (job_name ilike '%api%
+                        OR job_name ilike '%prod%ga%' 
                         AND is_scheduled = true
+                        )
                     ORDER BY 1
                 '''
                 )
         job_list = my_cur.fetchall()
-        job_df = pd.DataFrame(job_list,columns = ['Job_Name'])
+        job_df = pd.DataFrame(job_list,columns = ['Job_Name', 'JOB_LINK','DESCRIPTION'])
     return job_df
 
+# shows a table of current job names
 st.header('Scheduled dbt job - Minoro')
 st.title('View current scheduled dbt jobs')
 my_cnx = snowflake.connector.connect(**st.secrets['snowflake'])
@@ -24,3 +27,5 @@ st.dataframe(dbt_list,200,3000)
 
 my_cnx.close()
 
+# add description and job URL, button to choose which index number / job name and which column the info is for
+# add a colunm for showing the current job status
